a98bb3b3fbe95a09734363f3030a5f8599f266c1
Almir Zohorovic <64784654+brcak-zmaj@users.noreply.github.com>
2022-04-04 15:53:12 -0400

    Delete enable_change_PW.ps1

--- powershell/enable_change_PW.ps1
+++
@@ -1,4 +0,0 @@
-$users = Get-ADUser -Filter * -Properties CannotChangePassword -SearchBase "OU=OUNAME,DC=domain,DC=lan" | where { $_.CannotChangePassword -eq "false" }
-foreach( $user in $users){
- Set-ADUser $user -CannotChangePassword $True
-}
