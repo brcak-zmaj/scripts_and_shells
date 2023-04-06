19fc57dcb706a89ba5c97ff030d1b04200b5e649
A <64784654+brcak-zmaj@users.noreply.github.com>
2023-04-06 06:59:48 -0400

    Update Rejoin_Domain.ps1

--- Powershell/AD/Rejoin_Domain.ps1
+++ Powershell/AD/Rejoin_Domain.ps1
@@ -1,1 +1,14 @@
-netdom reset MachineName /domain domain.ad.com /User0 <UserID> /Password0 {Password | *}
+# Prompt the user for the domain name and administrator credentials
+$domain = Read-Host "Enter the domain name"
+$username = Read-Host "Enter the administrator username"
+$password = Read-Host "Enter the administrator password" -AsSecureString
+
+# Test the domain trust
+Test-ComputerSecureChannel -Credential (New-Object System.Management.Automation.PSCredential ($username, $password)) -Repair -Verbose
+
+# If the domain trust test fails, reset the computer account and rejoin the domain
+if ($LASTEXITCODE -ne 0) {
+  Reset-ComputerMachinePassword -Credential (New-Object System.Management.Automation.PSCredential ($username, $password)) -Server $domain -Verbose
+  Remove-Computer -UnjoinDomainCredential (New-Object System.Management.Automation.PSCredential ($username, $password)) -Verbose
+  Add-Computer -DomainName $domain -Credential (New-Object System.Management.Automation.PSCredential ($username, $password)) -Verbose
+}
